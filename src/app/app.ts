9c7894012d5443d8ad42b0decdcad155244a68b0
Richard Omoka <richard.omoka@gmail.com>
2025-10-23 16:21:57 +0300

    Whatever

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,20 +12,20 @@ export class App implements OnInit {
 
   constructor(private renderer: Renderer2) {}
    ngOnInit(): void {
-     let currentHost = window.location.hostname;
-    if (currentHost === 'localhost' || currentHost === '') {
-      currentHost = environment.DEFAULT_ADMIN_HOST_URL;
-    }
+    //  let currentHost = window.location.hostname;
+    // if (currentHost === 'localhost' || currentHost === '') {
+    //   currentHost = environment.DEFAULT_ADMIN_HOST_URL;
+    // }
 
-     console.log(`Current Host is ${currentHost}: Loading the theme css...`);
-    const linkElement = this.renderer.selectRootElement('#themecss', true);
-    // update the href attribute
-    this.renderer.setAttribute(
-      linkElement,
-      'href',
-      `${this.getThemeCssLink()}?tenantHost=${currentHost}`,
-    );
-    console.log(document.getElementById('themecss')?.getAttribute('href'));
+    //  console.log(`Current Host is ${currentHost}: Loading the theme css...`);
+    // const linkElement = this.renderer.selectRootElement('#themecss', true);
+    // // update the href attribute
+    // this.renderer.setAttribute(
+    //   linkElement,
+    //   'href',
+    //   `${this.getThemeCssLink()}?tenantHost=${currentHost}`,
+    // );
+    // console.log(document.getElementById('themecss')?.getAttribute('href'));
    }
   
    getThemeCssLink() {
